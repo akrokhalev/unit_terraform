dba5b5d111a29b7951f3d59ccaa64f450244408f
akrokhalev <aleksis5996@mail.ru>
2023-11-13 14:56:24 +0500

    gitignore

--- main.tf
+++ main.tf
@@ -1,2 +1,20 @@
-###
-hello world
+terraform {
+  required_providers {
+    yandex = {
+        source = "yandex-cloud/yandex"
+        version = "0.90.0"
+    }
+  }
+}
+
+locals {
+  folder_id = "b1gppsosmkmr6sm6ream"
+  cloud_id = "b1g3p7mlsl60dog8mj4t"
+}
+
+provider "yandex" {
+  cloud_id = local.cloud_id
+  folder_id = local.folder_id
+  service_account_key_file = ""
+  
+}
