78fef59348969a886f96c62a260d40368dc15d29
akrokhalev <aleksis5996@mail.ru>
2023-11-13 16:17:47 +0500

    1

--- main.tf
+++ main.tf
@@ -16,6 +16,47 @@ provider "yandex" {
   cloud_id = local.cloud_id
   folder_id = local.folder_id
   service_account_key_file = "/home/key/authorized_key.json"
+  zone = "ru-central1-a"
 
   
+}
+
+data "yandex_compute_image" "ubuntu_image" {
+  family = "ubuntu-2004-lts"
+}
+
+resource "yandex_compute_instance" "vm-test1" {
+  name = "test1"
+
+  resources {
+    cores  = 1
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_image.id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet_terraform.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./meta.yml")}"
+  }
+
+}
+
+resource "yandex_vpc_network" "network_terraform" {
+  name = "net_terraform"
+}
+
+resource "yandex_vpc_subnet" "subnet_terraform" {
+  name           = "sub_terraform"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network_terraform.id
+  v4_cidr_blocks = ["192.168.15.0/24"]
 }
